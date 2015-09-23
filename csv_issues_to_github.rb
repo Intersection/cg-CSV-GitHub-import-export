f7044f13aff567b13ba12ccb40eadb7105f10db7
Walter Mesquita <wapmesquita@gmail.com>
2015-09-23 18:58:28 -0300

    utilizando parametros

--- csv_issues_to_github.rb
+++ csv_issues_to_github.rb
@@ -11,35 +11,56 @@ require 'optparse'
 # BEGIN INTERACTIVE SECTION
 # Comment out this section (from here down to where the end is marked) if you want to use this interactively
 
-options = {}
-OptionParser.new do |opts|
+options = OpenStruct.new
+options.organization = ""
+options.repository = ""
+options.username = ""
+options.password = ""
+options.authkey = ""
+options.file = ""
+
+opts_parser = OptionParser.new do |opts|
   opts.banner = "Usage: example.rb [options]"
+  opts.separator ""
+  opts.separator "Specific options:"
 
   opts.on("-o", "--organization", "Define you repo's local") do |o|
-    options[:organization] << o
+    options.organization << o
   end
 
   opts.on("-r", "--repository", "Define you repo name") do |r|
-    options[:repository] << r
+    options.repository << r
   end
 
   opts.on("-u", "--username", "Your username") do |u|
-    options[:username] = u
+    options.username = u
   end
 
   opts.on("-p", "--password", "Your password") do |u|
-    options[:password] = u
+    options.password = u
   end
 
   opts.on("-k", "--authkey", "Your 40 char token") do |k|
-    options[:authkey] = k
+    options.authkey = k
   end
 
   opts.on("-f", "--file", "CSV file") do |f|
-    options[:file] << f
+    options.file << f
   end
+
+  opts.separator ""
+  opts.separator "Common options:"
+
+  # No argument, shows at tail.  This will print an options summary.
+  # Try it and see!
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+
 end
 
+opt_parser.parse!(ARGV)
 # END INTERACTIVE SECTION
 
 
