85e6bee63b00b6ded4a36497947dee6a2182d561
Steven Chanin <steven_chanin@alum.mit.edu>
2016-09-14 09:53:52 -0700

    cleanup whitespace in code

--- csv_issues_to_github.rb
+++ csv_issues_to_github.rb
@@ -11,31 +11,31 @@ require 'csv'
 # Comment out this section (from here down to where the end is marked) if you want to use this interactively
 
 puts "Username:"
-username = gets.chomp  
+username = gets.chomp
 if username == ""
 	abort("You need to supply a username. Thank you, come again.")
 end
 
 puts "Password:"
-password = gets.chomp  
+password = gets.chomp
 if password == ""
 	abort("You need to supply a password. Thank you, come again.")
 end
 
 puts "Path for the CSV file you want to use?"
-input_file = gets.chomp  
+input_file = gets.chomp
 if input_file == ""
 	abort("You need to supply a CSV file. Thank you, come again.")
 end
 
 puts "Organization?"
-org = gets.chomp  
+org = gets.chomp
 if org == ""
 	abort("You need to supply an organization. Thank you, come again.")
 end
 
 puts "Repository?"
-repo = gets.chomp  
+repo = gets.chomp
 if repo == ""
 	abort("You need to supply a repository. Thank you, come again.")
 end
@@ -50,7 +50,7 @@ end
 input_file = ""
 username = ""
 password = ""
-org = "" 
+org = ""
 repo = ""
 =end  # END HARD-CODED SECTION
 
@@ -63,7 +63,7 @@ csv = CSV.parse(csv_text, :headers => true)
 
 csv.each do |row|
 	client.create_issue(org_repo, row['title'], row['description'], options = {
-		:assignee => row['assignee_username'], 
+		:assignee => row['assignee_username'],
 		:labels => [row['label1'],row['label2'],row['label3']]})  #Add or remove label columns here.
 	puts "Imported issue:  #{row['title']}"
 end
